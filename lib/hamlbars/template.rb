24fb5f452708183e551ba4c5bafa127bfb7c8dcf
James Harton <jamesotron@gmail.com>
2012-01-10 13:30:40 +1300

    Make destination and compiler user modifyable.

--- lib/hamlbars/template.rb
+++ lib/hamlbars/template.rb
@@ -2,73 +2,99 @@ require 'tilt/template'
 
 module Hamlbars
   class Template < Tilt::Template
+
     JS_ESCAPE_MAP = {
       "\r\n"  => '\n',
       "\n"    => '\n',
       "\r"    => '\n',
       '"'     => '\\"',
-      "'"     => "\\'" }
+      "'"     => "\\'"
+    }
 
-      self.default_mime_type = 'application/javascript'
+    def self.template_destination
+      @template_destination ||= 'Handlebars.templates'
+    end
 
-      def self.engine_initialized?
-        defined? ::Haml::Engine
+    def self.template_destination=(x)
+      @template_destination = x
+    end
+
+    def self.template_compiler
+      @template_compiler ||= 'Handlebars.compile'
+    end
+
+    def self.template_compiler=(x)
+      @template_compiler = x
+    end
+
+    def self.template_partial_method
+      @template_partial_method ||= 'Handlebars.registerPartial'
+    end
+
+    def self.template_partial_method=(x)
+      @template_partial_method = x
+    end
+
+    self.default_mime_type = 'application/javascript'
+
+    def self.engine_initialized?
+      defined? ::Haml::Engine
+    end
+
+    def initialize_engine
+      require_template_library 'haml'
+    end
+
+    def prepare
+      options = @options.merge(:filename => eval_file, :line => line)
+      @engine = ::Haml::Engine.new(data, options)
+    end
+
+    def evaluate(scope, locals, &block)
+      template = if @engine.respond_to?(:precompiled_method_return_value, true)
+                   super
+                 else
+                   @engine.render(scope, locals, &block)
+                 end
+      if basename =~ /^_/
+        "#{self.class.template_partial_method}('#{name}', '#{template.strip.gsub(/(\r\n|[\n\r"'])/) { JS_ESCAPE_MAP[$1] }}');\n"
+      else
+          "#{self.class.template_destination}[\"#{scope.logical_path.downcase.gsub(/[^a-z0-9]/, '_')}\"] = #{self.class.template_compiler}(\"#{template.strip.gsub(/(\r\n|[\n\r"'])/) { JS_ESCAPE_MAP[$1] }}\");\n"
       end
+    end
 
-      def initialize_engine
-        require_template_library 'haml'
-      end
+    # Precompiled Haml source. Taken from the precompiled_with_ambles
+    # method in Haml::Precompiler:
+    # http://github.com/nex3/haml/blob/master/lib/haml/precompiler.rb#L111-126
+    def precompiled_template(locals)
+      @engine.precompiled
+    end
 
-      def prepare
-        options = @options.merge(:filename => eval_file, :line => line)
-        @engine = ::Haml::Engine.new(data, options)
-      end
-
-      def evaluate(scope, locals, &block)
-        template = if @engine.respond_to?(:precompiled_method_return_value, true)
-                     super
-                   else
-                     @engine.render(scope, locals, &block)
-                   end
-        if basename =~ /^_/
-        "Handlebars.registerPartial('#{name}', '#{template.strip.gsub(/(\r\n|[\n\r"'])/) { JS_ESCAPE_MAP[$1] }}');\n"
-        else
-          "Handlebars.templates[\"#{scope.logical_path.downcase.gsub(/[^a-z0-9]/, '_')}\"] = Handlebars.compile(\"#{template.strip.gsub(/(\r\n|[\n\r"'])/) { JS_ESCAPE_MAP[$1] }}\");\n"
-        end
-      end
-
-      # Precompiled Haml source. Taken from the precompiled_with_ambles
-      # method in Haml::Precompiler:
-      # http://github.com/nex3/haml/blob/master/lib/haml/precompiler.rb#L111-126
-      def precompiled_template(locals)
-        @engine.precompiled
-      end
-
-      def precompiled_preamble(locals)
-        local_assigns = super
-        @engine.instance_eval do
-          <<-RUBY
+    def precompiled_preamble(locals)
+      local_assigns = super
+      @engine.instance_eval do
+        <<-RUBY
           begin
             extend Haml::Helpers
             _hamlout = @haml_buffer = Haml::Buffer.new(@haml_buffer, #{options_for_buffer.inspect})
             _erbout = _hamlout.buffer
             __in_erb_template = true
             _haml_locals = locals
-          #{local_assigns}
+        #{local_assigns}
         RUBY
-        end
       end
+    end
 
-      def precompiled_postamble(locals)
-        @engine.instance_eval do
-          <<-RUBY
-          #{precompiled_method_return_value}
+    def precompiled_postamble(locals)
+      @engine.instance_eval do
+        <<-RUBY
+        #{precompiled_method_return_value}
           ensure
             @haml_buffer = @haml_buffer.upper
           end
         RUBY
-        end
       end
+    end
   end
 end
 
