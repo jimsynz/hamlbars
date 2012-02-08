50385e3a218eeed22d82a7e5c7973fdc8cf3773d
James Harton <jamesotron@gmail.com>
2012-02-09 11:49:26 +1300

    updated specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,9 @@ Bundler.setup
 Bundler.require
 
 require File.expand_path(__FILE__, '../lib/hamlbars.rb')
+
+RSpec.configure do |config|
+  config.debug = false
+  config.color_enabled = true
+  config.formatter = 'd'
+end
