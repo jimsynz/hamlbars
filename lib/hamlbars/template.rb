7357b5cc03087d6aa1d0a7c50e435050f30ad904
James Harton <jamesotron@gmail.com>
2012-11-04 19:55:48 +1300

    First pass at removing all reference to JavaScript and have Hamlbars just output Handlebars templates.

--- lib/hamlbars/template.rb
+++ lib/hamlbars/template.rb
@@ -2,74 +2,11 @@ require 'tilt/template'
 
 module Hamlbars
   class Template < Tilt::Template
-    include Ext::Closure
-    enable_closures! # Enable closures by default.
-    include Ext::Precompiler
     if defined? Rails
       include Ext::RailsHelper
-      enable_precompiler! if Rails.env.production?
     end
 
-    JS_ESCAPE_MAP = {
-      "\r\n"  => '\n',
-      "\n"    => '\n',
-      "\r"    => '\n',
-      '"'     => '\\"',
-      "'"     => "\\'"
-    }
-
-    # Used to change the asset path into a string which is
-    # safe to use as a JavaScript object property.
-    def self.path_translator(path)
-      path.downcase.gsub(/[^a-z0-9\/]/, '_')
-    end
-
-    # Handy helper to preconfigure Hamlbars to render for
-    # either :handlebars (default) or :ember.
-    def self.render_templates_for(whom=:handlebars)
-      if whom == :handlebars
-        self.template_destination = 'Handlebars.templates'
-        self.template_compiler = 'Handlebars.compile'
-        self.template_partial_method = 'Handlebars.registerPartial'
-      elsif whom == :ember
-        self.template_destination = 'Ember.TEMPLATES'
-        self.template_compiler = 'Ember.Handlebars.compile'
-        self.template_partial_method = 'Ember.Handlebars.registerPartial'
-      end
-    end
-
-    # The target object where the rendered template will
-    # be stored on the client side.
-    # Defaults to 'Handlebars.templates'
-    def self.template_destination
-      @template_destination ||= 'Handlebars.templates'
-    end
-
-    def self.template_destination=(x)
-      @template_destination = x
-    end
-
-    # The JavaScript function used to compile the HTML
-    # string into a usable Handlebars template.
-    def self.template_compiler
-      @template_compiler ||= 'Handlebars.compile'
-    end
-
-    def self.template_compiler=(x)
-      @template_compiler = x
-    end
-
-    # The JavaScript function used on the compile side to 
-    # register the template as a partial on the client side.
-    def self.template_partial_method
-      @template_partial_method ||= 'Handlebars.registerPartial'
-    end
-
-    def self.template_partial_method=(x)
-      @template_partial_method = x
-    end
-
-    self.default_mime_type = 'application/javascript'
+    self.default_mime_type = 'text/x-handlebars'
 
     def self.engine_initialized?
       defined? ::Haml::Engine
@@ -87,40 +24,14 @@ module Hamlbars
     # Uses Haml to render the template into an HTML string, then 
     # wraps it in the neccessary JavaScript to serve to the client.
     def evaluate(scope, locals, &block)
-      template = if @engine.respond_to?(:precompiled_method_return_value, true)
-                   super(scope, locals, &block)
-                 else
-                   @engine.render(scope, locals, &block)
-                 end
-
-      if scope.respond_to? :logical_path
-        path = scope.logical_path
+      if @engine.respond_to?(:precompiled_method_return_value, true)
+        super(scope, locals, &block)
       else
-        path = basename
+        @engine.render(scope, locals, &block)
       end
-
-      if basename =~ /^_/
-        name = partial_path_translator(path)
-        "#{self.class.template_partial_method}('#{name}', '#{template.strip.gsub(/(\r\n|[\n\r"'])/) { JS_ESCAPE_MAP[$1] }}');\n"
-      else
-        name = self.class.path_translator(path)
-        "#{self.class.template_destination}[\"#{name}\"] = #{self.class.template_compiler}(\"#{template.strip.gsub(/(\r\n|[\n\r"'])/) { JS_ESCAPE_MAP[$1] }}\");\n"
-      end
-    end
-
-    # Used to change the asset path into a string which is
-    # safe to use as a JavaScript object property. When
-    # the template is a partial (ie starts with a '_')
-    def partial_path_translator(path)
-      path = remove_underscore_from_partial_path(path)
-      self.class.path_translator(path).gsub(%r{/}, '.')
     end
 
     private
-
-    def remove_underscore_from_partial_path(path)
-      path.sub(/(.*)(\/|^)_(.+?)$/, '\1\2\3')
-    end
 
     # Precompiled Haml source. Taken from the precompiled_with_ambles
     # method in Haml::Precompiler:
